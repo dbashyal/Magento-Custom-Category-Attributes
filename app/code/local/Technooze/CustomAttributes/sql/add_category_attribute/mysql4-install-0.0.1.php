0d0967a301950185764040aea9e2d5485a403f50
damu <dbashyal@gmail.com>
2013-09-21 19:06:57 +1000

    add new field show_layered_nav in the category

---
+++ app/code/local/Technooze/CustomAttributes/sql/add_category_attribute/mysql4-install-0.0.1.php
@@ -0,0 +1,17 @@
+<?php
+$this->startSetup();
+$this->addAttribute('catalog_category', 'show_layered_nav', array(
+    'group'         => 'General',
+    'input'         => 'select',
+    'type'          => 'int',
+    'label'         => 'Show Layered Navigation',
+    'source'        => 'eav/entity_attribute_source_boolean',
+    'backend'       => '',
+    'visible'       => true,
+    'required'      => 0,
+    'visible_on_front' => true,
+    'global'        => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'sort_order'    => '99999',
+));
+
+$this->endSetup();
