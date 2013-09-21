75709c85c8463860b45db85b21fe55f8b3758a4b
damu <dbashyal@gmail.com>
2013-09-21 19:32:37 +1000

    added category image label

--- app/code/local/Technooze/CustomAttributes/sql/add_category_attribute/mysql4-install-0.0.1.php
+++ app/code/local/Technooze/CustomAttributes/sql/add_category_attribute/mysql4-install-0.0.1.php
@@ -1,5 +1,19 @@
 <?php
 $this->startSetup();
+$this->addAttribute('catalog_category', 'image_label', array(
+    'group'         => 'General Information',
+    'input'         => 'text',
+    'type'          => 'text',
+    'label'         => 'Image Alt Text',
+    'source'        => '',
+    'backend'       => '',
+    'visible'       => true,
+    'required'      => 0,
+    'visible_on_front' => true,
+    'global'        => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'sort_order'    => '99998',
+));
+
 $this->addAttribute('catalog_category', 'show_layered_nav', array(
     'group'         => 'General',
     'input'         => 'select',
